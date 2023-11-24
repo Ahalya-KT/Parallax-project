32eab0918241b7933f2c6f81806a2df69ab55ec8
Ahalya-KT <ahalyakt002@gmail.com>
2023-11-24 19:44:10 +0530

    parallex

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,52 @@
 import "./App.css";
 import { Parallax, ParallaxLayer } from "@react-spring/parallax";
+import ImageSlider from "./ImageSlider";
 
 function App() {
   return (
     <>
       <Parallax pages={2} style={{ top: "0", left: "0" }} className="animation">
-        <ParallaxLayer offset={0} speed={2.5}>
+        <ParallaxLayer offset={0.03} speed={0.1}>
           <div className="animation_layer parallax" id="artback"></div>
         </ParallaxLayer>
 
-        <ParallaxLayer offset={0} speed={0.1}>
+        <ParallaxLayer offset={0.025} speed={0.4}>
           <div className="animation_layer parallax" id="mountain"></div>
         </ParallaxLayer>
 
-        <ParallaxLayer offset={0} speed={0.3}>
+        <ParallaxLayer offset={0.02} speed={0.8}>
           <div className="animation_layer parallax" id="logoland"></div>
         </ParallaxLayer>
 
-        <ParallaxLayer offset={0} speed={0.35}>
+        <ParallaxLayer offset={0.015} speed={1.2}>
           <div className="animation_layer parallax" id="junglel"></div>
         </ParallaxLayer>
 
-        <ParallaxLayer offset={0} speed={0.5}>
+        <ParallaxLayer offset={0} speed={1.2}>
+          <div className="centered-text flex items-center justify-center ">
+            <h1 className=" font-bold text-slate-50 font-serif text-6xl py-56 ">
+              PARALLAX EFFECT
+              <p className="text-sm px-52 py-9">welcome to new ones</p>
+            </h1>
+          </div>
+        </ParallaxLayer>
+
+        <ParallaxLayer offset={0.01} speed={1.6}>
           <div className="animation_layer parallax" id="junglel2"></div>
         </ParallaxLayer>
 
-        <ParallaxLayer offset={0} speed={0.45}>
+        <ParallaxLayer offset={0} speed={2.0}>
           <div className="animation_layer parallax" id="junglel3"></div>
         </ParallaxLayer>
 
-        <ParallaxLayer offset={0} speed={0.4}>
+        <ParallaxLayer offset={0} speed={2.4}>
           <div className="animation_layer parallax" id="junglel4"></div>
         </ParallaxLayer>
       </Parallax>
+      {/* image slider */}
+      <div>
+        <ImageSlider />
+      </div>
     </>
   );
 }
---
+++ src/ImageSlider.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { Fade, Zoom, Slide } from "react-slideshow-image";
+import "react-slideshow-image/dist/styles.css";
+
+const slideImages = [
+  {
+    url: "https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg",
+    caption: "first one",
+  },
+
+  {
+    url: "https://img.freepik.com/premium-photo/reflection-lake-foot-mountains-evening-it-represents-natural-environment-silence-peace-created-with-generative-ai_529424-1333.jpg?size=626&ext=jpg&ga=GA1.1.1826414947.1699228800&semt=ais",
+    caption: "first one",
+  },
+
+  {
+    url: "https://img.freepik.com/premium-photo/discovering-lake-braies-simple-educational-insight-711_957479-3599.jpg",
+    caption: "first one",
+  },
+
+  {
+    url: "https://img.freepik.com/free-vector/background-with-natural-landscape-concept_23-2148650299.jpg",
+    caption: "first one",
+  },
+];
+
+//   adding styles
+const divStyle = {
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  height: "400px",
+  backgroundSize: "cover",
+};
+
+const spanStyle = {
+  fontSize: "20px",
+  background: "#efefef",
+  color: "#000000",
+};
+function ImageSlider() {
+  return (
+    <div className="slide-container">
+      <Fade>
+        {slideImages.map((image, index) => (
+          <div key={index}>
+            <div style={{ ...divStyle, backgroundImage: `url(${image.url})` }}>
+              <span style={{ spanStyle }}>{image.caption}</span>
+            </div>
+          </div>
+        ))}
+      </Fade>
+    </div>
+  );
+}
+
+export default ImageSlider;
